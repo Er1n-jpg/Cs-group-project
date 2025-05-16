b0cf9613d99698774081f8367798aa5768259096
Erin <enguy4@ocdsb.ca>
2025-05-16 12:16:19 -0400

    idol.java 1.2

--- Idol.java
+++ Idol.java
@@ -1,4 +1,65 @@
 public class Idol {
     public String name;
+        private int age;
+    private int height;
+    private int debutYear;
+    private static int numIdol;
+
+    public Idol(){
+        name  = "null";
+        age = 0;
+        height = 0;
+        debutYear = 0;
+        numIdol++;
+    }
+
+    public Idol(String na, int ag){
+      //  this(na,ag, 0, 0 );
+        numIdol++;
+    }
+
+    public Idol(String na, int ag, int ht, int dy){
+        name = na;
+        age = ag;
+        height = ht;
+        debutYear = dy;
+        numIdol++;
+    }
+
+    //acessor methods
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public int getHeight(){
+        return height;
+
+    }
+
+    public int getDebutYear(){
+        return debutYear;
+    }
+
+//mutator methods
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public String toString(){
+        return "name: " + name + "|| age: " + age + "|| height: " + height + "|| debut year: " + debutYear; 
+    }
+
     
 }
