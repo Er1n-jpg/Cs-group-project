bbf44b8a97b50a50fde515b04708fe250b56ebc1
Erin <enguy4@ocdsb.ca>
2025-05-17 15:03:53 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class main{
-
+    public static void main (String[] args){
+        Scanner console = new Scanner(System.in);
+        System.out.println("You have started a k-pop company! Hello CEO! pls enter your name");
+        String ceo = console.nextLine();
+        System.out.println("Please enter your company name");
+        String name = console.nextLine();
+        
+        Company c = new Company(ceo, name);
+        
+        System.out.println("Would you like to make a group?");
+        String answer = console.nextLine();
+        if (answer.equals("yes")){
+            
+        }
+    }
 }
